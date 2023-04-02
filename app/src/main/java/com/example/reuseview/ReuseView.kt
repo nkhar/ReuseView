5a032c589c58dbfa61da2ec9ea9de16d951d2ae1
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2023-04-02 22:33:24 +0400

    Added minimal scrolling by touching.

--- app/src/main/java/com/example/reuseview/ReuseView.kt
+++ app/src/main/java/com/example/reuseview/ReuseView.kt
@@ -2,14 +2,20 @@ package com.example.reuseview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import android.view.ViewGroup
-import androidx.core.view.ScrollingView
 import androidx.core.view.children
+import kotlin.math.max
+import kotlin.math.min
 
 class ReuseView : ViewGroup { //ScrollingView {
 
     companion object {
+
+        private const val TAG = " ReuseView"
 
         private val GAP_BETWEEN_CHILDREN = 30
 
@@ -20,10 +26,13 @@ class ReuseView : ViewGroup { //ScrollingView {
     private var childViewRetainers = arrayListOf<ViewRetainer>()
 
 
-    constructor(context: Context) : super(context)
+    constructor(context: Context) : this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        val vc = ViewConfiguration.get(context)
 
+        mTouchSlop = vc.scaledTouchSlop
+    }
 
 
     fun setAdapter(adapter: Adapter<*>) {
@@ -72,7 +81,7 @@ class ReuseView : ViewGroup { //ScrollingView {
             totalChildHeight += it.measuredHeight + GAP_BETWEEN_CHILDREN
         }
 
-        setMeasuredDimension(widthMeasureSpec, totalChildHeight)
+        setMeasuredDimension(widthMeasureSpec, 400)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
@@ -88,6 +97,58 @@ class ReuseView : ViewGroup { //ScrollingView {
             )
             startTop += child.measuredHeight + GAP_BETWEEN_CHILDREN
         }
+
+    }
+
+    private var mInitialTouchX = -1
+    private var mInitialTouchY = -1
+
+    private var mLastTouchX = -1
+    private var mLastTouchY = -1
+
+    private var mScrollPointerId = -1
+
+    private var mTouchSlop = 20
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+
+        val action: Int = event.actionMasked
+        val actionIndex: Int = event.actionIndex
+
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                mScrollPointerId = event.getPointerId(0)
+                mInitialTouchX = (event.x + 0.5f).toInt().also { mLastTouchX = it }
+                mInitialTouchY = (event.y + 0.5f).toInt().also { mLastTouchY = it }
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                val index: Int = event.findPointerIndex(mScrollPointerId)
+
+                if (index < 0) {
+                    Log.e(
+                        TAG, "Error processing scroll; pointer index for id "
+                                + mScrollPointerId + " not found. Did any MotionEvents get skipped?"
+                    )
+                    return false
+                }
+
+                val x: Int = (event.getX(index) + 0.5f).toInt()
+                val y: Int = (event.getY(index) + 0.5f).toInt()
+                val dx = mLastTouchX - x
+                var dy = mLastTouchY - y
+
+
+                dy = if (dy > 0) {
+                    max(0, dy - mTouchSlop)
+                } else {
+                    min(0, dy + mTouchSlop)
+                }
+
+                scrollBy(dy)
+            }
+        }
+        return true
 
     }
 
